f659336f84d6638872cfaf8c3f6190233ab0675c
Matias Gonzalez <matias_gonzalez@intranet.itresources>
2019-07-30 12:35:22 -0300

    nueva clase moto

---
+++ NuevaPruebaHibernate/src/main/java/ar/com/nuevapruebahibernate/anotation/MotorBike.java
@@ -0,0 +1,19 @@
+package ar.com.nuevapruebahibernate.anotation;
+
+public class MotorBike implements Vehiculos{	
+	
+	private String color;
+	
+	private int ruedas;
+	
+	public MotorBike() {
+		
+	}
+
+	@Override
+	public String damePatente() {
+		
+		return "KJH 123 y color: " + color;
+	}
+
+}
